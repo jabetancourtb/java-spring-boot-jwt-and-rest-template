138ea09e9ddc39becafd8d6eafb6e0ee59ea4c9f
jabetancourtb <jabetancourtb@hotmail.com>
2021-05-25 10:03:56 -0500

    Adjustments are made in the customer controller

--- api/src/main/java/com/api/infrastructure/controller/customer/CustomerController.java
+++ api/src/main/java/com/api/infrastructure/controller/customer/CustomerController.java
@@ -1,12 +1,12 @@
-package com.api.infrastructure.rest.controller.customer;
+package com.api.infrastructure.controller.customer;
 
 import com.api.domain.customer.Customer;
 import com.api.infrastructure.applicationservice.customer.CustomerApplicationService;
 import com.api.infrastructure.dto.customer.CustomerDTO;
 import com.api.infrastructure.dto.jwt.JwtDTO;
 import com.api.infrastructure.dto.signin.SignInDTO;
-import com.api.infrastructure.rest.client.RestClient;
 
+import com.api.infrastructure.restclient.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -53,14 +53,14 @@ public class CustomerController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CustomerDTO>getCustomerFromRestClient(@PathVariable("id") final long customerId) {
-        CustomerDTO customerDTO = null;
+    public ResponseEntity<Customer>getCustomerFromRestClient(@PathVariable("id") final long customerId) {
+        Customer customer = null;
         try {
-            customerDTO = restClient.getCustomerData(customerId);
-            return new ResponseEntity<>(customerDTO, HttpStatus.OK);
+            customer = restClient.getCustomerData(customerId);
+            return new ResponseEntity<>(customer, HttpStatus.OK);
         }
         catch(NoSuchElementException e) {
-            return new ResponseEntity<>(customerDTO, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(customer, HttpStatus.NOT_FOUND);
         }
     }
 
